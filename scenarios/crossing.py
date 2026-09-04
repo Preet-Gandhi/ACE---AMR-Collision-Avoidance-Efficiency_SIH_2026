84bd9b79a6eb9b117e8b2d489b95cef02cb7db42
prateeksinghchandel <prateeksinghchandel11@gmail.com>
2026-09-04 15:43:21 +0530

    task 1 complete

--- scenarios/crossing.py
+++ scenarios/crossing.py
@@ -3,5 +3,5 @@ from .reservation_helpers import build_world
 
 def build():
     # Both routes approach the center cell (4, 4) at the same timestep.
-    simulator, robots = build_world(starts=((4, 0), (4, 8)))
+    simulator, robots = build_world(starts=((0, (4, 0)), (1, (4, 8))))
     return simulator, robots, [[(4, 0), (4, 1), (4, 2), (4, 3), (4, 4)], [(4, 8), (4, 7), (4, 6), (4, 5), (4, 4)]]
--- scenarios/four_way_intersection.py
+++ scenarios/four_way_intersection.py
@@ -2,6 +2,6 @@ from .reservation_helpers import build_world
 
 
 def build():
-    simulator, robots = build_world(starts=((4, 0), (0, 4), (4, 8), (8, 4)))
+    simulator, robots = build_world(starts=((0, (4, 0)), (1, (0, 4)), (2, (4, 8)), (3, (8, 4))))
     paths = [[(4, 0), (4, 1), (4, 2), (4, 3), (4, 4)], [(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)], [(4, 8), (4, 7), (4, 6), (4, 5), (4, 4)], [(8, 4), (7, 4), (6, 4), (5, 4), (4, 4)]]
     return simulator, robots, paths
